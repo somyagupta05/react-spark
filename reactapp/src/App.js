d308938fae16f95277f6a5d93fbbd05394a7264f
somyagupta05 <somyagupta052003@gmail.com>
2025-03-18 23:56:49 +0530

    react setup

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -4,11 +4,18 @@ const App = () => {
   return (
     <>
     <div>
+      {/* component */}
+      <MyName/>
+
       <h1  className="myclassname"> welcome to my world {0+3}</h1>
     </div>
     <p>Awesome</p>
     </>
   )
+};
+// component
+const MyName=()=>{
+  return <h1>somya gupta</h1>
 }
 
 export default App;
