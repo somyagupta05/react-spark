90fac15ee5d7148f0ec7b47fb9d44f9dcbab632d
somyagupta05 <somyagupta052003@gmail.com>
2025-03-18 23:53:49 +0530

    react setup

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,11 +1,14 @@
-import React from 'react'
+// import React from 'react'
 
 const App = () => {
   return (
+    <>
     <div>
-      <h1> welcome to my world</h1>
+      <h1  className="myclassname"> welcome to my world {0+3}</h1>
     </div>
+    <p>Awesome</p>
+    </>
   )
 }
 
-export default App
+export default App;
