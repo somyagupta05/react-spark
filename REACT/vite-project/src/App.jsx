263fe092f5563682795d123745fca5998b089f7e
somyagupta05 <somyagupta052003@gmail.com>
2025-04-12 01:29:48 +0530

    completed lec 15

--- REACT/vite-project/src/App.jsx
+++ REACT/vite-project/src/App.jsx
@@ -1,19 +1,16 @@
 import { React } from "react";
+// default
+import NetflixSeries from "./components/NetflixSeries";
 // named export need to close in curly braces
-import { NetflixSeries } from "./components/NetflixSeries";
+// import { Footer,Header} from "./components/NetflixSeries";
 export const App = () => {
-  // Array with keys
-  // return([<NetflixSeries key="1"/> ,<NetflixSeries key="1"/> ,<NetflixSeries key="1"/> ]
+ 
   return (
     <>
+
       <NetflixSeries />
-      <NetflixSeries />
-      <NetflixSeries />
-      <NetflixSeries />
-      <NetflixSeries />
-      <NetflixSeries />
-      <NetflixSeries />
-      <NetflixSeries />
+      
+ 
     </>
   );
 };
